62fa9fc13cb8299d03382c991de9a52129f7a6b9
saharat wongyai <tkrunner30102@gmail.com>
2019-07-13 13:55:51 +0700

    Hello Github

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Saharat Wongyai'));
+app.get('/about', (req, res) => res.send('หน้า about'));
+app.get('/about2', (req, res) => res.send('เกี่ยวกับ'));
+app.get('/contact', (req, res) => res.send('ติดต่อเรา'));
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
