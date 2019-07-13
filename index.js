79c4caecede0ac1aae360cdb1eac41fc8faa4d4c
saharat wongyai <tkrunner30102@gmail.com>
2019-07-13 14:13:22 +0700

    เพิ่มหน้า test

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ app.get('/', (req, res) => res.send('Saharat Wongyai'));
 app.get('/about', (req, res) => res.send('หน้า about'));
 app.get('/about2', (req, res) => res.send('เกี่ยวกับ'));
 app.get('/contact', (req, res) => res.send('ติดต่อเรา'));
-
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+});
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
